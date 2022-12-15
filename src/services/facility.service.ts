339a9a3c27a47d2f6f0a8e7f90572accac29b236
prabhsharan36 <prabhsharan369@gmail.com>
2022-12-15 17:59:31 +0530

    refactor: facility service

--- src/services/facility.service.ts
+++ src/services/facility.service.ts
@@ -1,8 +1,9 @@
 import { container } from "../loaders/container";
-import _, { lowerCase } from "lodash";
+import _, { indexOf, lowerCase, times } from "lodash";
 import { DoctorVisit } from "../entity/doctorVisit.entity";
 import { getQueryManager } from "../core/dataLayes/manager";
 import { getImageUrl } from "./image.service";
+import { time } from "console";
 
 class FacilityService {
   protected facilityRepo = container.cradle.facitlityRepo;
@@ -85,21 +86,10 @@ class FacilityService {
   }
 
   async getOpenStatusAttribute(meta_data) {
-    let today_date = new Date();
-    let date =
-      today_date.getFullYear() +
-      "-" +
-      (today_date.getMonth() + 1) +
-      "-" +
-      today_date.getDate();
-    let time =
-      today_date.getHours() +
-      ":" +
-      today_date.getMinutes() +
-      ":" +
-      today_date.getSeconds();
-    let current_time = date + " " + time;
-    const weekday = [
+    let todayDate = new Date();
+
+    /** For sorting open hours in order */
+    const Weekdays = [
       "Sunday",
       "Monday",
       "Tuesday",
@@ -109,91 +99,109 @@ class FacilityService {
       "Saturday",
     ];
 
-    let today = weekday[today_date.getDay()];
-    let open_hours = meta_data?.open_hours || false;
-    let _24x7 = meta_data["24x7"] !== "undefined" ? meta_data["24x7"] : false;
+    let todayDay = Weekdays[todayDate.getDay()];
+
+    let openHours = _.isEmpty(meta_data?.open_hours)
+      ? null
+      : meta_data?.open_hours;
+
+    let _24x7 = meta_data?.["24x7"];
+
     if (!_24x7) {
-      let _sorted: any[] = [];
-      let weekdays: string[] = weekday;
-      let today = weekday[today_date.getDay()];
-      // console.log(meta_data.open_hours);
-
-      let _24x7 = meta_data["24x7"] !== "undefined" ? meta_data["24x7"] : false;
-      if (!_24x7) {
-        let _sorted: string[] = [];
-        let weekdays = weekday;
-        for (let day in Object.keys(weekdays)) {
-          console.log(
-            open_hours[Object.keys(open_hours).indexOf(weekdays[day])]
-          );
-
-          if (Object.keys(open_hours).indexOf(weekdays[day]) > -1) {
-            _sorted[day] =
-              open_hours[Object.keys(open_hours).indexOf(weekdays[day])];
-          }
-        }
-
-        open_hours = _sorted;
-        console.log(_sorted, "sorted");
-        let open_days = Object.keys(open_hours); // in sorted order.
-        // console.log(open_days, "sorted");
-        let t = weekdays.indexOf(today);
-        if (t in open_days) {
-          let closing_times = [];
-          let opening_times = [];
-
-          for (let time_slot in open_hours[t]) {
-            console.log(time_slot);
-
-            // closing_times.push(time_slot["close"]);
-            // opening_times.push(time_slot["open"]);
-          }
-          console.log(closing_times, "close");
-          console.log(opening_times, "open");
-        }
-
-        //     if (current_time < max(closing_times)) {
-        //         let _diff_in_next_open_time = 1440; // max value  =  no of minutes in 24hrs (i.e. 1440)
-        //         let _next_open_time = null;
-
-        //         for (let i = 0; i < (opening_times).length; i++) { // If the current time matches a open time slot.
-        //             if (opening_times[i] <= current_time && current_time < closing_times[i]) {
-        //                 let _diffInMinutes = current_time - closing_times[i];
-        //                 if (_diffInMinutes < 60) { // less then 1hour before closing the facility
-        //                     return 'Closes in ' + _diffInMinutes + ' ' + (
-        //                     _diffInMinutes > 1 ? 'minutes' : 'minute'
-        //                 );
-        //                 } else if (_diffInMinutes < 120) { // less then 2 hours before closing the facility
-        //                     return 'Open till '.str_replace(':00', '', closing_times[i].format('g:i a')); // using open_hours to get beautified close time string.
-        //                 } else {
-        //                     return 'Open Now';
-        //                 }
-        //             } else { // data if the current time is not in any of the open slots
-        //                 if (opening_times[i] > current_time) {
-        //                     let _diff = current_time.diffInMinutes(opening_times[i]);
-        //                     if (_diff < _diff_in_next_open_time) {
-        //                         _diff_in_next_open_time = _diff;
-        //                         _next_open_time = opening_times[i];
-        //                     }
-        //                 }
-        //             }
-        //         }
-
-        //         if (_diff_in_next_open_time < 60) {
-        //             return 'Opens in '._diff_in_next_open_time.' '.(_diff_in_next_open_time > 1 ? 'minutes' : 'minute');
-        //         } else {
-        //             _next_open_time = str_replace(':00', '', _next_open_time.format('g:i a'));
-        //             return 'Opens at '._next_open_time;
-        //         }
-        //     } else {
-        //         return this.nextAvailibilityStr(open_hours, open_days);
-        //     }
-        // } else {
-        //     return this.nextAvailibilityStr(open_hours, open_days);
-        // }
-      } else {
-        return "Open 24x7";
+      let sorted = {};
+      if (openHours) {
+        Weekdays.forEach((day: string) => {
+          sorted[day] = openHours?.[day] || null;
+        });
       }
+      openHours = sorted;
+      let openDays = Object.keys(openHours); // in sorted order.
+
+      /* CHECK IF OPENS TODAY */
+      if (openDays.indexOf(todayDay) > -1) {
+        let openingTimes: number[] = [];
+        let closingTimes: number[] = [];
+
+        for (let timeSlot in openHours[todayDay]) {
+          openingTimes.push(parseInt(openHours[todayDay][timeSlot]?.open));
+          closingTimes.push(parseInt(openHours[todayDay][timeSlot]?.close));
+        }
+        // console.log(closingTimes);
+        const CurrentTime = parseInt(
+          todayDate
+            .toLocaleString("en-US", {
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true,
+            })
+            .toLocaleLowerCase()
+        );
+
+        console.log(
+          closingTimes,
+          Math.max(...closingTimes),
+          "Math.max(...closingTimes)"
+        );
+        if (CurrentTime < Math.max(...closingTimes)) {
+          let diffInNextOpenTime = 1440; // max value  =  no of minutes in 24hrs (i.e. 1440)
+          let nextOpenTime;
+          for (let index = 0; index < openingTimes.length; index++) {
+            const openTime = openingTimes[index];
+            if (
+              openingTimes[index] <= CurrentTime &&
+              CurrentTime < closingTimes[index]
+            ) {
+              // TODO (Find Difference)
+              // let diffInMinutes = $current_time->diffInMinutes($closing_times[$i]);
+              let diffInMinutes = 60;
+              if (diffInMinutes < 60) {
+                // less then 1hour before closing the facility
+                return `Closes in ${diffInMinutes} ${
+                  diffInMinutes > 1 ? "minutes" : "minute"
+                }`;
+              } else if (diffInMinutes < 120) {
+                // less then 2 hours before closing the facility
+                return `Open till `;
+                // str_replace(':00', '', $closing_times[$i]->format('g:i a')); // using $open_hours to get beautified close time string.
+              } else {
+                return "Open Now";
+              }
+            } else {
+              // data if the current time is not in any of the open slots
+              if (openingTimes[index] > CurrentTime) {
+                // TODO
+                let diff = 0;
+                // $_diff = $current_time->diffInMinutes($opening_times[$i]);
+                if (diff < diffInNextOpenTime) {
+                  diffInNextOpenTime = diff;
+                  nextOpenTime = openingTimes[index];
+                }
+              }
+            }
+          }
+
+          if (diffInNextOpenTime < 60) {
+            // TODO
+            // return 'Opens in ' .
+            // 	$_diff_in_next_open_time .
+            // 	' ' .
+            // 	($_diff_in_next_open_time > 1 ? 'minutes' : 'minute');
+          } else {
+            // TODO
+            // $_next_open_time = str_replace(':00', '', $_next_open_time->format('g:i a'));
+            // return 'Opens at ' . $_next_open_time;
+          }
+          // If the current time matches a open time slot.
+        } else {
+          // TODO
+          // return $this->nextAvailibilityStr($open_hours, $open_days);
+        }
+      } else {
+        // TODO
+        // return $this->nextAvailibilityStr($open_hours, $open_days);
+      }
+    } else {
+      return "Open 24x7";
     }
   }
 
